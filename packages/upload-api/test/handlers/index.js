8cdae6bbdb849c6bb2ea59ba67dad18470ae7774
Alan Shaw <alan@storacha.network>
2025-03-17 15:32:08 +0000

    fix: re-add removed IPNI service

--- packages/upload-api/test/handlers/index.js
+++ packages/upload-api/test/handlers/index.js
@@ -9,6 +9,74 @@ import * as Result from '../helpers/result.js'
 
 /** @type {API.Tests} */
 export const test = {
+  'index/add should publish index to IPNI service': async (assert, context) => {
+    const { proof, spaceDid } = await registerSpace(alice, context)
+    const contentCAR = await randomCAR(32)
+    const contentCARBytes = new Uint8Array(await contentCAR.arrayBuffer())
+
+    const connection = connect({
+      id: context.id,
+      channel: createServer(context),
+    })
+
+    // upload the content CAR to the space
+    await uploadBlob(
+      {
+        connection,
+        issuer: alice,
+        audience: context.id,
+        with: spaceDid,
+        proofs: [proof],
+      },
+      {
+        digest: contentCAR.cid.multihash,
+        bytes: contentCARBytes,
+      }
+    )
+
+    const index = await fromShardArchives(contentCAR.roots[0], [
+      contentCARBytes,
+    ])
+    const indexCAR = Result.unwrap(await index.archive())
+    const indexLink = await CAR.link(indexCAR)
+
+    // upload the index CAR to the space
+    await uploadBlob(
+      {
+        connection,
+        issuer: alice,
+        audience: context.id,
+        with: spaceDid,
+        proofs: [proof],
+      },
+      {
+        digest: indexLink.multihash,
+        bytes: indexCAR,
+      }
+    )
+
+    const indexAdd = IndexCapabilities.add.invoke({
+      issuer: alice,
+      audience: context.id,
+      with: spaceDid,
+      nb: { index: indexLink },
+      proofs: [proof],
+    })
+    const receipt = await indexAdd.execute(connection)
+    Result.try(receipt.out)
+
+    // ensure a result exists for the content root
+    assert.ok(
+      Result.unwrap(await context.ipniService.query(index.content.multihash))
+    )
+
+    for (const shard of index.shards.values()) {
+      for (const slice of shard.entries()) {
+        // ensure a result exists for each multihash in the index
+        assert.ok(Result.unwrap(await context.ipniService.query(slice[0])))
+      }
+    }
+  },
   'index/add should fail if index is not stored in agent space': async (
     assert,
     context
