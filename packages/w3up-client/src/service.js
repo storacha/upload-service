e28e986d303fdd34730ace03e794bf5443a22eff
Felipe Forbeck <felipe.forbeck@gmail.com>
2025-04-04 09:53:46 -0300

    feat(access-client): allow custom headers in Agent

--- packages/w3up-client/src/service.js
+++ packages/w3up-client/src/service.js
@@ -1,3 +1,5 @@
+import { isBrowser, isNode, isBun, isDeno, isElectron } from 'environment'
+
 import * as client from '@ucanto/client'
 import { CAR, HTTP } from '@ucanto/transport'
 import * as DID from '@ipld/dag-ucan/did'
@@ -6,35 +8,68 @@ import { receiptsEndpoint } from '@storacha/upload-client'
 export const accessServiceURL = new URL('https://up.storacha.network')
 export const accessServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const accessServiceConnection = client.connect({
-  id: accessServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+const envName = isBrowser ? 'Browser' : isNode ? 'Node' : isBun ? 'Bun' : isDeno ? 'Deno' : isElectron ? 'Electron' : 'Unknown'
+export const defaultHeaders = {
+  'X-Client': `Storacha/1 (js; ${envName})`,
+}
+
+/**
+ * @param {object} [options]
+ * @param {Record<string, any>} [options.headers]
+ */
+export const accessServiceConnection = (options = {}) =>
+  client.connect({
+    id: accessServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: accessServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+    ...options,
+  })
 
 export const uploadServiceURL = new URL('https://up.storacha.network')
 export const uploadServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const uploadServiceConnection = client.connect({
-  id: uploadServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+/**
+ * @param {object} [options]
+ * @param {Record<string, any>} [options.headers]
+ */
+export const uploadServiceConnection = (options = {}) =>
+  client.connect({
+    id: uploadServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: accessServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+  })
 
 export const filecoinServiceURL = new URL('https://up.storacha.network')
 export const filecoinServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const filecoinServiceConnection = client.connect({
-  id: filecoinServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+/**
+ * @param {object} [options]
+ * @param {Record<string, any>} [options.headers]
+ */
+export const filecoinServiceConnection = (options = {}) =>
+  client.connect({
+    id: filecoinServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: filecoinServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+  })
 
-/** @type {import('./types.js').ServiceConf} */
-export const serviceConf = {
-  access: accessServiceConnection,
-  upload: uploadServiceConnection,
-  filecoin: filecoinServiceConnection,
-}
+/** @type {() => import('./types.js').ServiceConf} */
+export const serviceConf = () => ({
+  access: accessServiceConnection(),
+  upload: uploadServiceConnection(),
+  filecoin: filecoinServiceConnection(),
+})
 
 export { receiptsEndpoint }
