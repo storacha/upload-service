8671f6b845e972794208e8e41982a42a122e57ec
Felipe Forbeck <fforbeck@gmail.com>
2025-04-04 12:48:25 -0300

    feat(access-client): allow custom headers in the agent (#215)
    ### Changes
    - Updated the `@ucanto/transport` lib to `9.2.0` - which supports custom
    headers in HTTP transport channels
    - Updated the Access client to support custom headers in the channel
    creation, connection, and Agent constructor.
    - Tests
    
    ---------
    
    Co-authored-by: ash <alan@storacha.network>

--- packages/w3up-client/src/service.js
+++ packages/w3up-client/src/service.js
@@ -1,3 +1,5 @@
+import { isBrowser, isNode, isBun, isDeno, isElectron } from 'environment'
+
 import * as client from '@ucanto/client'
 import { CAR, HTTP } from '@ucanto/transport'
 import * as DID from '@ipld/dag-ucan/did'
@@ -6,35 +8,85 @@ import { receiptsEndpoint } from '@storacha/upload-client'
 export const accessServiceURL = new URL('https://up.storacha.network')
 export const accessServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const accessServiceConnection = client.connect({
-  id: accessServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+/* c8 ignore start */
+const envName = isBrowser
+  ? 'Browser'
+  : isNode
+  ? 'Node'
+  : isBun
+  ? 'Bun'
+  : isDeno
+  ? 'Deno'
+  : isElectron
+  ? 'Electron'
+  : 'Unknown'
+export const defaultHeaders = {
+  'X-Client': `Storacha/1 (js; ${envName})`,
+}
+/* c8 ignore end */
+
+/**
+ * @param {object} [options]
+ * @param {Record<string, string>} [options.headers]
+ * @param {import('./types.js').Principal} [options.id]
+ * @param {URL} [options.url]
+ */
+export const accessServiceConnection = (options = {}) =>
+  client.connect({
+    id: options.id ?? accessServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: options.url ?? accessServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+  })
 
 export const uploadServiceURL = new URL('https://up.storacha.network')
 export const uploadServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const uploadServiceConnection = client.connect({
-  id: uploadServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+/**
+ * @param {object} [options]
+ * @param {Record<string, string>} [options.headers]
+ * @param {import('./types.js').Principal} [options.id]
+ * @param {URL} [options.url]
+ */
+export const uploadServiceConnection = (options = {}) =>
+  client.connect({
+    id: options.id ?? uploadServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: options.url ?? uploadServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+  })
 
 export const filecoinServiceURL = new URL('https://up.storacha.network')
 export const filecoinServicePrincipal = DID.parse('did:web:up.storacha.network')
 
-export const filecoinServiceConnection = client.connect({
-  id: filecoinServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open({ url: accessServiceURL, method: 'POST' }),
-})
+/**
+ * @param {object} [options]
+ * @param {Record<string, string>} [options.headers]
+ * @param {import('./types.js').Principal} [options.id]
+ * @param {URL} [options.url]
+ */
+export const filecoinServiceConnection = (options = {}) =>
+  client.connect({
+    id: options.id ?? filecoinServicePrincipal,
+    codec: CAR.outbound,
+    channel: HTTP.open({
+      url: options.url ?? filecoinServiceURL,
+      method: 'POST',
+      headers: { ...defaultHeaders, ...options.headers },
+    }),
+  })
 
-/** @type {import('./types.js').ServiceConf} */
-export const serviceConf = {
-  access: accessServiceConnection,
-  upload: uploadServiceConnection,
-  filecoin: filecoinServiceConnection,
-}
+/** @type {() => import('./types.js').ServiceConf} */
+export const serviceConf = () => ({
+  access: accessServiceConnection(),
+  upload: uploadServiceConnection(),
+  filecoin: filecoinServiceConnection(),
+})
 
 export { receiptsEndpoint }
