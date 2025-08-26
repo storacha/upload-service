08f1945edf8155b12119870c48abccec64ec5794
Felipe Forbeck <fforbeck@gmail.com>
2025-08-26 13:56:39 -0300

    fix: selective re-exports instead of wildcard

--- packages/ui/packages/react/src/index.ts
+++ packages/ui/packages/react/src/index.ts
@@ -1,4 +1,13 @@
-export * from '@storacha/ui-core'
+// Re-export specific items from ui-core
+export { 
+  Client,
+  Account, 
+  Space,
+  type ContextState,
+  type ContextActions,
+  type ServiceConfig
+} from '@storacha/ui-core'
+
 export * from './providers/Provider.js'
 export * from './Authenticator.js'
 export * from './Uploader.js'
