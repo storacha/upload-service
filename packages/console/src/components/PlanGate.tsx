22016c1fefacf827f4626f3bc3195a668158921e
Akash Jana <akash@Akashs-MacBook-Air.local>
2025-09-01 13:38:14 +0530

    fix: Added Back(logout) button on Pricing Page

--- packages/console/src/components/PlanGate.tsx
+++ packages/console/src/components/PlanGate.tsx
@@ -2,13 +2,17 @@
 
 import { ReactNode, useMemo } from 'react'
 import { useW3 } from '@storacha/ui-react'
-import StripePricingTable, { StripeTrialPricingTable, SSOIframeStripePricingTable } from './PricingTable'
+import StripePricingTable, {
+  StripeTrialPricingTable,
+  SSOIframeStripePricingTable,
+} from './PricingTable'
 import { TopLevelLoader } from './Loader'
 import { Logo } from '@/brand'
 import { useConditionalPlan } from '@/hooks'
 import { useSearchParams } from 'next/navigation'
 import { useIframe } from '@/contexts/IframeContext'
 import { useRecordRefcode } from '@/lib/referrals/hooks'
+import Link from 'next/link'
 
 const PricingTable = ({
   email,
@@ -18,48 +22,64 @@ const PricingTable = ({
   referredBy?: string
 }) => {
   const { isIframe } = useIframe()
-  
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen relative">
       <div className="my-6">
         <Logo />
       </div>
+      <Link
+        href="https://storacha.console.network/logout"
+        className="absolute top-4 right-4 bg-hot-red text-white px-4 py-2 rounded-lg shadow-md hover:border-white transition"
+      >
+        &larr; Back
+      </Link>
       <div className="max-w-screen-lg font-epilogue text-black text-center bg-white border border-hot-red rounded-2xl overflow-hidden p5 mx-4 mb-4">
         {referredBy ? (
           <>
             <div className="px-6 py-6 lg:px-24">
               <h1 className="my-4 font-bold">Welcome, {email}!</h1>
               <p className="my-4">
-                Congratulations! You are eligible for a free trial of our Lite or
-                Business subscriptions. That means we won&apos;t charge you
-                anything today. If you choose a Lite plan, you will get two months
-                for free! If you choose Business, you will get one month for free!
-                We do need you to provide a valid credit card before we can start
-                your trial - pick a plan below and complete the checkout flow to
-                get started!
+                Congratulations! You are eligible for a free trial of our Lite
+                or Business subscriptions. That means we won&apos;t charge you
+                anything today. If you choose a Lite plan, you will get two
+                months for free! If you choose Business, you will get one month
+                for free! We do need you to provide a valid credit card before
+                we can start your trial - pick a plan below and complete the
+                checkout flow to get started!
               </p>
               <p className="my-4">
                 Please note that after your free trial ends, you will be charged
-                10 USD per month for Lite or 100 USD per month for Business tier.
+                10 USD per month for Lite or 100 USD per month for Business
+                tier.
               </p>
             </div>
-            {isIframe ? <SSOIframeStripePricingTable /> : <StripeTrialPricingTable />}
+            {isIframe ? (
+              <SSOIframeStripePricingTable />
+            ) : (
+              <StripeTrialPricingTable />
+            )}
           </>
         ) : (
           <>
             <div className="px-6 py-6 lg:px-24">
               <h1 className="my-4 font-bold">Welcome, {email}!</h1>
               <p className="my-4">
-                To get started you&apos;ll need to sign up for a subscription. If
-                you choose the starter plan we won&apos;t charge your credit card,
-                but we do need a card on file before we will store your bits.
+                To get started you&apos;ll need to sign up for a subscription.
+                If you choose the starter plan we won&apos;t charge your credit
+                card, but we do need a card on file before we will store your
+                bits.
               </p>
               <p className="my-4">
                 Pick a plan below and complete the Stripe checkout flow to get
                 started!
               </p>
             </div>
-            {isIframe ? <SSOIframeStripePricingTable /> : <StripePricingTable />}
+            {isIframe ? (
+              <SSOIframeStripePricingTable />
+            ) : (
+              <StripePricingTable />
+            )}
           </>
         )}
       </div>
