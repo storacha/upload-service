8286b6d1b1870b055d685161f43400aa4bb763ea
ash <alan@storacha.network>
2025-09-04 06:33:19 +0100

    feat: output timestamps in JSON upload listing (#411)

--- packages/cli/lib.js
+++ packages/cli/lib.js
@@ -171,7 +171,9 @@ export async function readProofFromBytes(bytes) {
 export function uploadListResponseToString(res, opts = {}) {
   if (opts.json) {
     return res.results
-      .map(({ root, shards }) => dagJSON.stringify({ root, shards }))
+      .map(({ root, shards, insertedAt, updatedAt }) =>
+        dagJSON.stringify({ root, shards, insertedAt, updatedAt })
+      )
       .join('\n')
   } else if (opts.shards) {
     return res.results
