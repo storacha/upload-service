79337ec648a89836f96e3b8979e75a8c61d73634
alikhere <alikhurshid.dev@gmail.com>
2025-09-16 20:20:38 +0530

    feat: update sign-up-in example to use StorachaAuth

--- packages/ui/examples/react/sign-up-in/tailwind.config.js
+++ packages/ui/examples/react/sign-up-in/tailwind.config.js
@@ -6,7 +6,30 @@ export default {
     '../components/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hot-red': '#E91315',
+        'hot-red-light': '#EFE3F3',
+        'hot-yellow': '#FFC83F',
+        'hot-yellow-light': '#FFE4AE',
+        'hot-blue': '#0176CE',
+        'hot-blue-light': '#BDE0FF'
+      },
+      fontFamily: {
+        'epilogue': ['Epilogue', 'sans-serif'],
+      },
+      animation: {
+        'spin': 'spin 1s linear infinite',
+        'bgPosDrift': 'bgPosDrift 60s ease infinite',
+      },
+      keyframes: {
+        bgPosDrift: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      }
+    },
   },
   plugins: [],
 }
