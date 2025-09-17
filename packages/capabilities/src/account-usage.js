a8a527b75d8f547953d52aa131bf6850e108542f
hannahhoward <hannah@hannahhoward.net>
2025-09-16 21:46:16 -0700

    feat(capabilities): add account-usage capabilities

---
+++ packages/capabilities/src/account-usage.js
@@ -0,0 +1,62 @@
+import { capability, fail, ok, Schema } from '@ucanto/validator'
+import {
+  AccountDID,
+  SpaceDID,
+  equalWith,
+  and,
+  equal,
+  containedWithin,
+} from './utils.js'
+
+/**
+ * Capability can only be delegated (but not invoked) allowing audience to
+ * be derived any `account-usage/` prefixed capability for the account identified
+ * by DID in the `with` field.
+ */
+export const accountUsage = capability({
+  can: 'account-usage/*',
+  with: AccountDID,
+  derives: equalWith,
+})
+
+/**
+ * Capability can be invoked by an agent to retrieve usage data for all or a
+ * specified set of spaces within an account in a given period.
+ */
+export const get = capability({
+  can: 'account-usage/get',
+  with: AccountDID,
+  nb: Schema.struct({
+    spaces: SpaceDID.array().optional(),
+    /** Period to retrieve events between. */
+    period: Schema.struct({
+      /** Time in seconds after Unix epoch (inclusive). */
+      from: Schema.integer().greaterThan(-1),
+      /** Time in seconds after Unix epoch (exclusive). */
+      to: Schema.integer().greaterThan(-1),
+    }).optional(),
+  }),
+  derives: (child, parent) => {
+    return (
+      and(equalWith(child, parent)) ||
+      and(
+        (() => {
+          if (parent.nb.spaces === undefined) {
+            return ok({})
+          }
+          if (child.nb.spaces === undefined) {
+            return fail(
+              `Constraint violation: violates imposed spaces constraint ${parent.nb.spaces} because it asks for all spaces`
+            )
+          }
+          return containedWithin(child.nb.spaces, parent.nb.spaces, 'spaces')
+        })()
+      ) ||
+      and(
+        equal(child.nb.period?.from, parent.nb.period?.from, 'period.from')
+      ) ||
+      and(equal(child.nb.period?.to, parent.nb.period?.to, 'period.to')) ||
+      ok({})
+    )
+  },
+})
---
+++ packages/capabilities/test/capabilities/account-usage.test.js
@@ -0,0 +1,326 @@
+import assert from 'assert'
+import { access } from '@ucanto/validator'
+import { Verifier } from '@ucanto/principal'
+import * as AccountUsage from '../../src/account-usage.js'
+import * as Capability from '../../src/top.js'
+import {
+  alice,
+  service,
+  bobAccount as account,
+  mallory,
+  space,
+  bob,
+} from '../helpers/fixtures.js'
+import {
+  createAuthorization,
+  delegateWithAttestation,
+  validateAuthorization,
+} from '../helpers/utils.js'
+
+const top = () =>
+  delegateWithAttestation(Capability.top, {
+    account: account.did(),
+    service,
+    audience: alice,
+    with: account.did(),
+  })
+
+const accountUsage = () =>
+  delegateWithAttestation(AccountUsage.accountUsage, {
+    account: account.did(),
+    service,
+    audience: alice,
+    with: account.did(),
+  })
+
+describe('account-usage/get capabilities', function () {
+  it('account-usage/get can be derived can be derived from *', async () => {
+    const get = AccountUsage.get.invoke({
+      issuer: alice,
+      audience: service,
+      with: account.did(),
+      nb: {},
+      proofs: await top(),
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+    assert.ok(result.ok)
+    assert.deepEqual(result.ok.audience.did(), service.did())
+    assert.equal(result.ok.capability.can, AccountUsage.get.can)
+  })
+
+  it('account-usage/get can be derived can be derived from account-usage/*', async () => {
+    const get = AccountUsage.get.invoke({
+      issuer: alice,
+      audience: service,
+      with: account.did(),
+      nb: {},
+      proofs: await accountUsage(),
+    })
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.ok)
+    assert.deepEqual(result.ok.audience.did(), service.did())
+    assert.equal(result.ok.capability.can, AccountUsage.get.can)
+  })
+
+  it('account-usage/get should fail when escalating period constraint', async () => {
+    const period = { from: 5, to: 6 }
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: { period },
+      proofs: await top(),
+    })
+
+    {
+      const get = AccountUsage.get.invoke({
+        issuer: mallory,
+        audience: service,
+        with: account.did(),
+        nb: { period: { from: period.from + 1, to: period.to } },
+        proofs: [delegation],
+      })
+
+      const result = await access(await get.delegate(), {
+        capability: AccountUsage.get,
+        principal: Verifier,
+        authority: service,
+        validateAuthorization,
+      })
+
+      assert.ok(result.error)
+      assert(
+        result.error.message.includes(
+          `${period.from + 1} violates imposed period.from constraint ${
+            period.from
+          }`
+        )
+      )
+    }
+
+    {
+      const get = AccountUsage.get.invoke({
+        issuer: mallory,
+        audience: service,
+        with: account.did(),
+        nb: { period: { from: period.from, to: period.to + 1 } },
+        proofs: [delegation],
+      })
+
+      const result = await access(await get.delegate(), {
+        capability: AccountUsage.get,
+        principal: Verifier,
+        authority: service,
+        validateAuthorization,
+      })
+
+      assert.ok(result.error)
+      assert(
+        result.error.message.includes(
+          `${period.to + 1} violates imposed period.to constraint ${period.to}`
+        )
+      )
+    }
+  })
+
+  it('account-usage/get period from must be an int', async () => {
+    const period = { from: 5.5, to: 6 }
+    const proofs = await top()
+    assert.throws(() => {
+      AccountUsage.get.invoke({
+        issuer: alice,
+        audience: service,
+        with: account.did(),
+        nb: { period },
+        proofs,
+      })
+    }, /Expected value of type integer instead got 5\.5/)
+  })
+
+  it('account-usage/get period to must be an int', async () => {
+    const period = { from: 5, to: 6.6 }
+    const proofs = await top()
+    assert.throws(() => {
+      AccountUsage.get.invoke({
+        issuer: alice,
+        audience: service,
+        with: account.did(),
+        nb: { period },
+        proofs,
+      })
+    }, /Expected value of type integer instead got 6\.6/)
+  })
+
+  it('account-usage/get succeeds when spaces are equal in delegation and invocation', async () => {
+    const spaces = [space.did()]
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: { spaces },
+      proofs: await top(),
+    })
+
+    const get = AccountUsage.get.invoke({
+      issuer: mallory,
+      audience: service,
+      with: account.did(),
+      nb: { spaces },
+      proofs: [delegation],
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.ok)
+    assert.deepEqual(result.ok.audience.did(), service.did())
+    assert.equal(result.ok.capability.can, AccountUsage.get.can)
+    assert.deepEqual(result.ok.capability.nb, { spaces })
+  })
+
+  it('account-usage/get succeeds when delegation has no spaces constraint but invocation specifies spaces', async () => {
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: {},
+      proofs: await top(),
+    })
+
+    const spaces = [space.did()]
+    const get = AccountUsage.get.invoke({
+      issuer: mallory,
+      audience: service,
+      with: account.did(),
+      nb: { spaces },
+      proofs: [delegation],
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.ok)
+    assert.deepEqual(result.ok.audience.did(), service.did())
+    assert.equal(result.ok.capability.can, AccountUsage.get.can)
+    assert.deepEqual(result.ok.capability.nb, { spaces })
+  })
+
+  it('account-usage/get should fail when delegation has spaces constraint but invocation asks for all spaces', async () => {
+    const spaces = [space.did()]
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: { spaces },
+      proofs: await top(),
+    })
+
+    const get = AccountUsage.get.invoke({
+      issuer: mallory,
+      audience: service,
+      with: account.did(),
+      nb: {},
+      proofs: [delegation],
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.error)
+    assert(
+      result.error.message.includes(
+        `Constraint violation: violates imposed spaces constraint ${spaces} because it asks for all spaces`
+      )
+    )
+  })
+
+  it('account-usage/get succeeds when invocation asks for subset of delegation spaces', async () => {
+    const delegationSpaces = [space.did(), bob.did()]
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: { spaces: delegationSpaces },
+      proofs: await top(),
+    })
+
+    const invocationSpaces = [space.did()]
+    const get = AccountUsage.get.invoke({
+      issuer: mallory,
+      audience: service,
+      with: account.did(),
+      nb: { spaces: invocationSpaces },
+      proofs: [delegation],
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.ok)
+    assert.deepEqual(result.ok.audience.did(), service.did())
+    assert.equal(result.ok.capability.can, AccountUsage.get.can)
+    assert.deepEqual(result.ok.capability.nb, { spaces: invocationSpaces })
+  })
+
+  it('account-usage/get should fail when invocation asks for spaces not in delegation constraint', async () => {
+    const delegationSpaces = [space.did()]
+    const delegation = await AccountUsage.get.delegate({
+      issuer: alice,
+      audience: mallory,
+      with: account.did(),
+      nb: { spaces: delegationSpaces },
+      proofs: await top(),
+    })
+
+    const invocationSpaces = [space.did(), bob.did()]
+    const get = AccountUsage.get.invoke({
+      issuer: mallory,
+      audience: service,
+      with: account.did(),
+      nb: { spaces: invocationSpaces },
+      proofs: [delegation],
+    })
+
+    const result = await access(await get.delegate(), {
+      capability: AccountUsage.get,
+      principal: Verifier,
+      authority: service,
+      validateAuthorization,
+    })
+
+    assert.ok(result.error)
+    assert(
+      result.error.message.includes(
+        `Constraint violation: ${invocationSpaces} violates imposed spaces constraint ${delegationSpaces} because it contains items not in the constraint`
+      )
+    )
+  })
+})
