04d5ab92a9933a2cf4a7865f6062966fb908402a
Vicente Olmedo <vicente@storacha.network>
2026-01-28 13:01:16 +0100

    feat: add limit property to plan/get successful result

--- packages/capabilities/src/types.ts
+++ packages/capabilities/src/types.ts
@@ -1084,6 +1084,7 @@ export type PlanGet = InferInvokedCapability<typeof PlanCaps.get>
 export interface PlanGetSuccess {
   updatedAt: ISO8601Date
   product: DID
+  limit: string
 }
 
 export interface PlanNotFound extends Ucanto.Failure {
