e90c229921203394ed916d4a8573cc778204c398
Travis Vachon <travis.vachon@gmail.com>
2026-01-28 16:41:14 -0800

    fix: a couple more things to remove

--- packages/upload-api/src/lib.js
+++ packages/upload-api/src/lib.js
@@ -21,7 +21,6 @@ import { createService as createPlanService } from './plan.js'
 import { createService as createUsageService } from './usage.js'
 import { createService as createLegacyW3sService } from './web3.storage.js'
 import { createService as createFilecoinService } from '@storacha/filecoin-api/storefront/service'
-import { createService as createLegacyAdminService } from '@web3-storage/upload-api/admin'
 import { createService as createAccountUsageService } from './account/usage.js'
 import * as AgentMessage from './utils/agent-message.js'
 
@@ -181,12 +180,7 @@ export const createService = (context) => ({
   customer: createCustomerService(context),
   provider: createProviderService(context),
   'rate-limit': createRateLimitService(context),
-  admin: {
-    ...createAdminService(context),
-    // @ts-expect-error `uploadTable` items now have a `cause` field. This does
-    // not matter since `admin/store/inspect` handler does not use this table.
-    store: createLegacyAdminService(context).store,
-  },
+  admin: createAdminService(context),
   space: createSpaceService(context),
   subscription: createSubscriptionService(context),
   upload: createUploadService(context),
